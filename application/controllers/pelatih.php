114189de0b71826da33ad7712dc58a1df8414b07
isepmm <isepmm@gmail.com>
2017-05-03 19:45:45 +0700

    pelatih controller
    mengedit fungsi dalam pelatih controller

--- application/controllers/pelatih.php
+++ application/controllers/pelatih.php
@@ -11,17 +11,22 @@ class pelatih extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('homePelatih');
+		$this->load->view(' upload_form_khusus');
 	}
 
 	public function createInformasi()
 	{
-		
+		$this->load->view('upload_form_khusus');
+	}
+
+	public function homePelatih()
+	{
+		$this->load->view('homeTeacher');
 	}
 
 	public function beritaKhusus()
 	{
-		$this->load->view('upload_form', array('error' => ' ' ));
+		$this->load->view('upload_form_khusus', array('error' => ' ' ));
 	}
 
 	public function do_upload(){
@@ -36,18 +41,18 @@ class pelatih extends CI_Controller {
                 {
                         $error = array('error' => $this->upload->display_errors());
 
-                        $this->load->view('upload_form', $error);
+                        $this->load->view('upload_form_khusus', $error);
                 }
                 else
                 {
                         $data = array('upload_data' => $this->upload->data());
 
                         $this->load->view('upload_success', $data);
+                        $this->load->view('upload_form_khusus', $data);
                 }
 
 	}
 	
-
 }
 
 /* End of file admin.php */
